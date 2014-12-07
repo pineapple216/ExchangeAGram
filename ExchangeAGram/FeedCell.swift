fddc5e390a4192221dc951a66980b0892800451e
Koen Hendriks <koenhendriks89@gmail.com>
2014-12-07 12:38:57 +0100

    Added a UICollectionViewCell to the storyboard and classes to manage it

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Koen Hendriks on 07/12/14.
+//  Copyright (c) 2014 Koen Hendriks. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var captionLabel: UILabel!
+}
