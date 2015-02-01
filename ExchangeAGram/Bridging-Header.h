9025e4889dcc941d74b4bc7a9488168a5c9f84a8
Koen Hendriks <koenhendriks89@gmail.com>
2015-02-01 12:01:35 +0100

    Added the facebookSDK

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Koen Hendriks on 01/02/15.
+//  Copyright (c) 2015 Koen Hendriks. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
